c1d8b6ae41f90eb880e2675c98e77c79a4ee8d11
dbazin00 <dbazin00@fesb.hr>
2020-01-26 10:17:15 +0100

    Ubačene ikone socijalnih mreža u kontaktu

--- src/pages/kontakt.js
+++ src/pages/kontakt.js
@@ -2,6 +2,16 @@ import React from "react"
 import Layout from "../components/layout"
 import SEO from "../components/seo"
 
+import styles from "../styles/kontakt.module.css"
+
+import facebook from "../images/social-media/facebook.png"
+import linkedin from "../images/social-media/linkedin.png"
+import whatsapp from "../images/social-media/whatsapp.png"
+import twitter from "../images/social-media/twitter.png"
+import instagram from "../images/social-media/instagram.png"
+import youtube from "../images/social-media/youtube.png"
+import github from "../images/social-media/github.png"
+
 const Kontakt = () => {
   const contactItems = [
     {
@@ -25,14 +35,40 @@ const Kontakt = () => {
       text: "021/xxx-xxx",
     },
   ]
+
+  const socialMedia = [
+    { icon: facebook, link: "https://www.facebook.com/" },
+    { icon: linkedin, link: "https://www.linkedin.com/" },
+    { icon: whatsapp, link: "https://www.whatsapp.com/" },
+    { icon: twitter, link: "https://twitter.com/" },
+    { icon: instagram, link: "https://www.instagram.com/" },
+    { icon: youtube, link: "https://www.youtube.com/" },
+    { icon: github, link: "https://github.com/" },
+  ]
   return (
     <div>
       <SEO title="Kontakt" keywords="Kontakt" />
       <Layout header={"Kontakt"} activePage={"/kontakt"}>
         {contactItems.map(item => (
-          <div style={{ marginTop: "25px" }} key={item.text}>
+          <div className={styles.contact} key={item.text}>
             <span className="material-icons">{item.icon}</span> {item.text}
           </div>
+        ))}
+        {socialMedia.map(social => (
+          <a
+            href={social.link}
+            key={social.link}
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            <img
+              src={social.icon}
+              alt="Slika nije pronađena"
+              width="35px"
+              height="35px"
+              className={styles.social}
+            />
+          </a>
         ))}
       </Layout>
     </div>
